df3b9bc7cc3a91b1c67ea92be0a3677660b97240
Bina <96680892+binababar@users.noreply.github.com>
2022-01-27 09:24:23 -0600

    updated assignments

---
+++ src/com/myassignments/Product.java
@@ -0,0 +1,24 @@
+package com.myassignments;
+
+public class Product {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num;
+		String pname;
+		display();
+		
+
+	}
+
+	private static void display() {
+		// TODO Auto-generated method stub
+		int num=100;
+		String pname="tv";
+		System.out.println(+num +"\t" +"pname");
+		
+		
+	}
+
+}
